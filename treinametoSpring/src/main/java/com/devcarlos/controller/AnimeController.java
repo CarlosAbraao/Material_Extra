b8007cec4a7792b64d98114a47ddf1cb2aa32c25
Carlos Abraao <carloschavesnd@hotmail.com>
2021-12-14 09:48:38 -0300

    alguns macetes pra criar metodos e manipular o codigo

--- treinametoSpring/src/main/java/com/devcarlos/controller/AnimeController.java
+++ treinametoSpring/src/main/java/com/devcarlos/controller/AnimeController.java
@@ -4,9 +4,10 @@ package com.devcarlos.controller;
 import com.devcarlos.entities.Anime;
 import com.devcarlos.service.AnimeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,12 +17,28 @@ import java.util.List;
 public class AnimeController {
 
     // VARIAVEL LOCAL SERVICE
-
+    // USANDO O REQUIREDARGSCONS...  EU N PRECISO USAR O AUTOWIRED
+    // A CLASSE A SER ESTANCIADA DEVE SER SINALIZADA COM "FINAL"
     private final AnimeService animeService;
 
 
     @GetMapping
-    public List<Anime> list(){
-        return animeService.allList();
+    public ResponseEntity<List<Anime>> list() {
+       // return new ResponseEntity<>(animeService.allList(), HttpStatus.OK); UMA DAS FORMAS DE FAZER
+         return ResponseEntity.ok(animeService.allList());
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Anime> fyndById(@PathVariable long id) {
+        // return new ResponseEntity<>(animeService.allList(), HttpStatus.OK);
+        return ResponseEntity.ok(animeService.findById(id));
+
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED) // POSSO FAZER DESSA FORMA OU DA FORMA LA EMBAIXO PAR AMOSTRAR O STATUS
+    public ResponseEntity<Anime> save(@RequestBody Anime anime){
+       return new ResponseEntity<>(animeService.save(anime),HttpStatus.CREATED);
     }
 }
--- treinametoSpring/src/main/java/com/devcarlos/service/AnimeService.java
+++ treinametoSpring/src/main/java/com/devcarlos/service/AnimeService.java
@@ -1,13 +1,43 @@
 package com.devcarlos.service;
 
 import com.devcarlos.entities.Anime;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AnimeService {
+public static List<Anime> animes;
 
-    public List<Anime> allList(){
-        return List.of(new Anime(1l,"Naturo"), new Anime(2l,"Sakura"), new Anime(3l,"Garfield"));
-    }}
+static {
+    animes = new ArrayList<>(List.of(new Anime(1l,"Naturo"), new Anime(2l,"Sakura"), new Anime(3l,"Garfield")));
+}
+    public  List<Anime> allList(){
+
+    return animes;
+    }
+
+
+    public Anime findById(Long id){
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
+    }
+
+
+    public Anime save(Anime anime) {
+
+    // GERANDO UM NUMERO ALEATORIO E INSERINDO NO ID
+    anime.setId(ThreadLocalRandom.current().nextLong(3,500));
+    animes.add(anime);
+    return anime;
+
+
+
+    }
+}
